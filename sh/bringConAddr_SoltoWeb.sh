c71fd849c87646ed4c9d0beb9de9912bffecb7cc
Eom Hyun Jun <zzp1318@gmail.com>
2024-05-27 19:26:54 +0900

    💚 prettier 추가 / shell 경로, 변수 수정

--- sh/bringConAddr_SoltoWeb.sh
+++ sh/bringConAddr_SoltoWeb.sh
@@ -5,7 +5,7 @@ source ./sh/config.sh
 npm --prefix ./lottery-sol run ta > $SAVE_FILE
 
 contract_address=$(grep 'contract address' $SAVE_FILE | awk '{print $4}')
-sed -i '' "s|export const contract_addr =.*|export const contract_addr = \"$contract_address\";|" $DEST_PATH
+sed -i '' "s|export const contract_addr: string =.*|export const contract_addr: string = \"$contract_address\";|" $DEST_PATH
 
 echo "====================="
 echo "Contract address 변경완료: $contract_address"
